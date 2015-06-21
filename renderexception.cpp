be4235f904f57804800a6b0b8eff4d94b524a4f8
ssi <sergeev.s.i1991@gmail.com>
2015-06-22 01:17:00 +0600

    c++9x standart

--- renderexception.cpp
+++ renderexception.cpp
@@ -15,7 +15,7 @@ RenderException::RenderException(std::string _m, std::string _f, std::string _fi
 
 const char* RenderException::what() const throw()
 {
-	std::string line_str = std::to_string(line);
+	std::string line_str = (char*)line;
 	return (nameOfException + ": " + message + " in " + file + "(" + func + ": " + line_str + ")").c_str();
 }
 
